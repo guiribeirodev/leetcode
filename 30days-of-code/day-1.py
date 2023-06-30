3fccef3381cf87f4624edfb216c49abcab2727cc
guiribeirodev <guiribeirodev@gmail.com>
2023-06-29 22:15:25 -0300

    Add day 1 challenge

---
+++ 30days-of-code/day-1.py
@@ -0,0 +1,22 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+# intNumber = 2 ex int
+# floatNumber = 6.0 ex float
+# word = "" ex string
+
+# Read and save an integer, double, and String to your variables.
+intNumber = int(input())
+floatNumber = float(input())
+word = input()
+
+# Print the sum of both integer variables on a new line.
+print(intNumber + i)
+
+# Print the sum of the double variables on a new line.
+print( floatNumber + d)
+
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(s + word)
