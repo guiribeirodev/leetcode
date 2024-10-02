f4a6b9bd8afcd0c903eb456db356f7c0f57679be
guiribeirodev <guiribeirodev@gmail.com>
2024-10-02 15:52:41 -0300

    Add day 11

---
+++ 30_days_of_code/day11.js
@@ -0,0 +1,49 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+
+
+function main() {
+
+    let arr = Array(6);
+
+    for (let i = 0; i < 6; i++) {
+        arr[i] = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+    }
+    
+    let maxSum = -Infinity;
+
+    for (let i = 0; i < 4; i++) {
+        for (let j = 0; j < 4; j++) {
+            let currentLine = arr[i][j] + arr[i][j + 1] + arr[i][j + 2];
+            let halfLine = arr[i + 1][j + 1];
+            let bottomLine = arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2];
+            
+            let sumHourglass = currentLine + halfLine + bottomLine;
+            if (sumHourglass > maxSum) {
+                maxSum = sumHourglass;
+            }
+        }
+    }
+
+    console.log(maxSum);
+}
